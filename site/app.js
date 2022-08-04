3d911e07b06d7e0deb9731b35af1874b7f44938c
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-04 15:11:33 +1200

    moving files around

--- site/app.js
+++ site/app.js
@@ -44,12 +44,46 @@ fetch(BIRDS_URL)
     })
     .catch((error) => console.log(error))
 
+/**
+ * Adds a list of birds to the page
+ *
+ * @param birds array containing json objects describing which birds to display
+ */
 function addBirdsToMain(birds) {
     // TODO
 }
 
+/**
+ * Creates an html info box for a particular that is to be added to the page
+ * then adds it using DOM
+ *
+ * @param birdJSON json object describing a bird to be added
+ */
 function createBirdInfoBox(birdJSON) {
     // TODO
+    let infoCard = document.createElement("div")
+    infoCard.className = "bird-info-card"
+    let birdImg = document.createElement("img")
+    birdImg.setAttribute("class", `bird-image`)
+    birdImg.setAttribute("alt", `Photo of ${birdJSON.english_name}`)
+
+    let factSheet = document.createElement("div")
+    factSheet.setAttribute("class", "bird-fact-sheet")
+
+    let description = document.createElement("p")
+    description.setAttribute("class", "description")
+
+    let photoCredit = document.createElement("p")
+    description.setAttribute("class", "photo-credit")
+
+}
+
+/**
+ * Finds the name of the jpeg image for a particular bird to be displayed
+ * @param birdEnglishName
+ */
+function findBirdImg(birdEnglishName) {
+    birdEnglishName = birdEnglishName.toLowerCase()
 }
 
 // Adding birds to main
@@ -68,33 +102,39 @@ function getSearchInfo() {
     return {"searchString": searchString, "conservationStatus": conservationStatus, "sortBy": conservationStatus}
 }
 
+/**
+ * Handles event when a search button is requested from a user
+ */
 function searchButtonPressed() {
     // TODO handle case where birds json has not been loaded
     if (birds === null) {
         console.log("birds haven't been loaded yet!")
         return
     }
-    const searchQuery = getSearchInfo()
-    if (searchQuery === "") {
+    const searchInfo = getSearchInfo()
+    const searchString = searchInfo.searchString
+    if (searchString === "") {
         return
     }
     let birdsFiltered = birds.filter((birdJSON) => {
-        filterBird(searchQuery, birdJSON)
+        filterBird(searchString, birdJSON)
     })
+
+    // TODO order birds
 }
 
 /**
  * Filter function for which birds to show, according to a search query (string) from a user
  *
- * @param searchQuery string for a search query from a user
+ * @param searchString string for a search query from a user
  * @param birdJSON json object for a particular bird to be filtered or not
  * @returns {boolean} if the bird should be kept on the  page or not
  */
-function filterBird(searchQuery, birdJSON) {
+function filterBird(searchString, birdJSON) {
     let names = [birdJSON.primary_name, birdJSON.english_name, birdJSON.scientific_name]
     for (let name of names) {
         // TODO sort out normalisation
-        if (name.toLowerCase().includes(searchQuery.toLowerCase())) {
+        if (name.toLowerCase().includes(searchString.toLowerCase())) {
             return true
         }
     }
